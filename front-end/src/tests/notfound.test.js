49a3efad33afaf386b18093bbb1ff3bbe3cda33c
Ludson96 <ludson_ps27@hotmail.com>
2023-03-11 18:28:09 -0300

    test: :white_check_mark: Tests for front-end page NotFound

---
+++ front-end/src/tests/notfound.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('NotFound page', () => {
+
+  it('Verificar', async () => {
+      renderWithRouter(<App />, ['/rotaerrada']);
+
+      const notFountTitle = screen.getByText('Not Found');
+
+      expect(notFountTitle).toBeInTheDocument();
+    });
+});
